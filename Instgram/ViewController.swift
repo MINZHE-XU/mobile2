1f8d75a2a13171f90a08a638aa15cb8f32e8d03d
MINZHE-XU <mazin.xu@gmail.com>
2018-09-17 22:53:04 +1000

    Initial Commit

---
+++ Instgram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instgram
+//
+//  Created by 许敏哲 on 17/9/18.
+//  Copyright © 2018 许敏哲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
